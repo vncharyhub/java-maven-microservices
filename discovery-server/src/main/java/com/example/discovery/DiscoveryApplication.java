87422d1d66908a22b669fcbd75e7429e46435581
DevOps_Culture_in_SDLC <90929082+vncharyhub@users.noreply.github.com>
2025-06-05 06:48:15 +0530

    Create DiscoveryApplication.java

---
+++ discovery-server/src/main/java/com/example/discovery/DiscoveryApplication.java
@@ -0,0 +1,7 @@
+@SpringBootApplication
+@EnableEurekaServer
+public class DiscoveryApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(DiscoveryApplication.class, args);
+    }
+}
