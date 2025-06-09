824fbf52367f8ffa69a842516ff899fc29c3251e
DevOps_Culture_in_SDLC <90929082+vncharyhub@users.noreply.github.com>
2025-06-09 13:23:37 +0530

    Update vpc.tf

--- terraform/vpc.tf
+++ terraform/vpc.tf
@@ -5,7 +5,7 @@ module "vpc" {
   version = ">=3.0"
   name = var.cluster_name
   cidr = "10.0.0.0/16"
-  azs = data.aws_availability_zones.azs.names[0:2]
+  azs = slice(data.aws_availability_zones.azs.names, 0, 2)
   public_subnets = ["10.0.1.0/24","10.0.2.0/24"]
   private_subnets = ["10.0.3.0/24","10.0.4.0/24"]
 }
