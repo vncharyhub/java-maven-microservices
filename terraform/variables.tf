47c635a6d1430381809cc3824432ad89d09d5b7f
DevOps_Culture_in_SDLC <90929082+vncharyhub@users.noreply.github.com>
2025-06-09 12:17:02 +0530

    Update variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,9 +1,3 @@
-variable "cluster_name" {
-  type        = string
-  description = "EKS cluster name"
-  default     = "microservices-cluster"
-}
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
+variable "cluster_name"    { type=string, default="microservices-eks" }
+variable "aws_region"      { type=string, default="us-east-1" }
+variable "jenkins_ssh_iam_user_arn" { type=string, description="IAM User ARN allowed to assume role for kubectl access" }
