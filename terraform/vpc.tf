26dc8ccb91d5c1ebeccd02bf33d4e8cfb8eb7873
DevOps_Culture_in_SDLC <90929082+vncharyhub@users.noreply.github.com>
2025-06-09 12:04:46 +0530

    Create vpc.tf

---
+++ terraform/vpc.tf
@@ -0,0 +1,9 @@
+module "vpc" {
+  source  = "terraform-aws-modules/vpc/aws"
+  version = ">= 3.0"
+  name = var.cluster_name
+  cidr = "10.0.0.0/16"
+  azs = slice(data.aws_availability_zones.available.names, 0, 2)
+  public_subnets  = ["10.0.1.0/24","10.0.2.0/24"]
+  private_subnets = ["10.0.3.0/24","10.0.4.0/24"]
+}
